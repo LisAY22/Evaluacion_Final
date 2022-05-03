683c37e04c7c9c334de5dab2ab335679b780d771
Elisa Ajxup Yax <lisajxupyax@gmail.com>
2022-05-03 09:46:00 -0600

    Cambios en main.py
    En esta parte se agrego un pequeño programa a main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print("Datos de Estudiante:")
 print("Nombre: Elisa Yanira Ajxup Yax")
 print("Carné: 1553222")
+
+print("Hola")
+print("Ingrese su animal favorito")
+print("Ingreselo todo en minuscula, porfavor")
+AnimalUsuario=input()
+Animal="perro"
+if AnimalUsuario==Animal:
+    print("A mi también me gusta el",Animal)
+else:
+    print("Es un animal interesante, pero me gusta más el",Animal)
